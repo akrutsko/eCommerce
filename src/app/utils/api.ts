772537fc4a3416044ec3773ad482789e3f51d74b
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-08-03 15:53:08 +0300

    feat: add file api.ts

---
+++ src/app/utils/api.ts
@@ -0,0 +1,29 @@
+// Full access
+// const CTP_PROJECT_KEY = 'peakpulse';
+// const CTP_CLIENT_SECRET = 'WgPk-nmbKj2DD1Dk9Aq4m_gFe_Lrv4WO';
+// const CTP_CLIENT_ID = 'aSH1Qs-X58nIAdx6Dbrw-pvz';
+// const CTP_AUTH_URL = 'https://auth.europe-west1.gcp.commercetools.com';
+// const CTP_API_URL = 'https://api.europe-west1.gcp.commercetools.com';
+// const CTP_SCOPES = 'manage_project:peakpulse';
+
+// export default class Api {
+//   static getAccessToken() {
+//     const myHeaders = new Headers();
+//     myHeaders.append('Authorization',
+// 'Basic YVNIMVFzLVg1OG5JQWR4NkRicnctcHZ6OldnUGstbm1iS2oyREQxRGs5QXE0bV9nRmVfTHJ2NFdP');
+
+//     const raw = '';
+
+//     const requestOptions: RequestInit = {
+//       method: 'POST',
+//       headers: myHeaders,
+//       body: raw,
+//       redirect: 'follow',
+//     };
+
+//     fetch('https://auth.europe-west1.gcp.commercetools.com/oauth/token?grant_type=client_credentials', requestOptions)
+//       .then((response) => response.text())
+//       .then((result) => console.log(result))
+//       .catch((error) => console.log('error', error));
+//   }
+// }
