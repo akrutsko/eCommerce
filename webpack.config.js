3acee37b5b493c2be3028acade4697cde526a944
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-08-05 11:32:11 +0300

    chore: configure css-minimizer, update dev configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,84 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-const devMode = process.env.NODE_ENV !== 'production';
-const devServer = devMode
-  ? { devtool: 'inline-source-map', devServer: { port: 8080, historyApiFallback: true } }
-  : {};
-const esLintPlugin = devMode ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })];
+module.exports = (env, argv) => {
+  const devMode = argv.mode !== 'production';
+  const devServer = devMode
+    ? {
+        devtool: 'inline-source-map',
+        devServer: {
+          port: 8080,
+          historyApiFallback: true,
+          watchFiles: './src/index.html',
+        },
+      }
+    : {};
+  const esLintPlugin = devMode ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })];
+  const cssMinimizerPlugin = devMode
+    ? {}
+    : {
+        optimization: {
+          minimizer: [new CssMinimizerPlugin()],
+        },
+      };
 
-module.exports = {
-  entry: {
-    index: './src/index.ts',
-  },
-  output: {
-    clean: true,
-    filename: '[name].[contenthash].js',
-    path: path.join(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[hash][ext]',
-  },
-  resolve: {
-    extensions: ['.ts', '.js', 'json', '...'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/i,
-        use: 'ts-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.svg$/i,
-        type: 'asset/source',
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
+  return {
+    entry: {
+      index: './src/index.ts',
+    },
+    output: {
+      clean: true,
+      filename: '[name].[contenthash].js',
+      path: path.join(__dirname, 'dist'),
+      assetModuleFilename: 'assets/[hash][ext]',
+    },
+    resolve: {
+      extensions: ['.ts', '.js', 'json', '...'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/i,
+          use: 'ts-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.svg$/i,
+          type: 'asset/source',
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        favicon: './src/assets/favicon.ico',
+        minify: {
+          html5: true,
+          minifyCSS: true,
+          minifyJS: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+      ...esLintPlugin,
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      favicon: './src/assets/favicon.ico',
-      minify: {
-        html5: true,
-        minifyCSS: true,
-        minifyJS: true,
-        collapseWhitespace: true,
-        removeComments: true,
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-    ...esLintPlugin,
-  ],
-  ...devServer,
+    ...devServer,
+    ...cssMinimizerPlugin,
+  };
 };
