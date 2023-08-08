763b161bc80d6ce84233f6417045db71d2f6e020
Svitlana Moiseienko <106593720+Luchiweb@users.noreply.github.com>
2023-08-08 08:11:55 +0200

    feat: add basic styles for components (#11)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/index.html', './src/**/*.ts'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--main-color)",
+        "secondary-color": "var(--secondary-color)",
+        "tertiary-color": "var(--tertiary-color)",
+        "bg-color": "var(--bg-color)"
+      },
+    },
   },
   plugins: [],
 };
