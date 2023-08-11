4934d30d3bd99a7cbd0e9930fc4e6225dbd26a74
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-08-11 11:48:28 +0300

    refactor: chain in appendNode, rename ul in linkList

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -26,23 +26,19 @@ class Header {
       href: '#',
       html: logotype,
     });
-    nav.appendNode(logo);
 
     const mobileMenu = new ElementCreator({
       tag: 'div',
       classes:
         'mobile-menu md:w-full md:max-w-full max-w-[390px] hidden justify-between md:flex gap-8',
     });
-    nav.appendNode(mobileMenu);
 
-    const ul = new ElementCreator({
+    const linksList = new ElementCreator({
       tag: 'ul',
       classes: 'items-center justify-between flex gap-5',
     });
-    mobileMenu.appendNode(ul);
 
     const liHome = new ElementCreator({ tag: 'li' });
-    ul.appendNode(liHome);
 
     const aHome = new ElementAnchorCreator({
       href: '#',
@@ -52,7 +48,6 @@ class Header {
     liHome.appendNode(aHome);
 
     const liAboutUs = new ElementCreator({ tag: 'li' });
-    ul.appendNode(liAboutUs);
 
     const aAboutUs = new ElementAnchorCreator({
       href: '#',
@@ -62,23 +57,21 @@ class Header {
     liAboutUs.appendNode(aAboutUs);
 
     const tab = new ElementCreator({ tag: 'li', classes: 'relative group tab' });
-    ul.appendNode(tab);
+    linksList.appendNode(liHome).appendNode(liAboutUs).appendNode(tab);
 
     const aCategories = new ElementAnchorCreator({
       href: '#',
       text: 'Categories',
       classes: 'h4 hover:text-primary-color',
     });
-    tab.appendNode(aCategories);
 
     const submenu = new ElementCreator({
       tag: 'ul',
       classes: 'submenu absolute hidden bg-white px-2 py-1 w-max',
     });
-    tab.appendNode(submenu);
+    tab.appendNode(aCategories).appendNode(submenu);
 
     const liSummerTime = new ElementCreator({ tag: 'li' });
-    submenu.appendNode(liSummerTime);
 
     const aSummerTime = new ElementAnchorCreator({
       href: '#',
@@ -88,7 +81,6 @@ class Header {
     liSummerTime.appendNode(aSummerTime);
 
     const liPeakClimber = new ElementCreator({ tag: 'li' });
-    submenu.appendNode(liPeakClimber);
 
     const aPeakClimber = new ElementAnchorCreator({
       href: '#',
@@ -98,7 +90,6 @@ class Header {
     liPeakClimber.appendNode(aPeakClimber);
 
     const liBallGames = new ElementCreator({ tag: 'li' });
-    submenu.appendNode(liBallGames);
 
     const aBallGames = new ElementAnchorCreator({
       href: '#',
@@ -108,7 +99,10 @@ class Header {
     liBallGames.appendNode(aBallGames);
 
     const liIceAdventures = new ElementCreator({ tag: 'li' });
-    submenu.appendNode(liIceAdventures);
+    submenu.appendNode(liSummerTime)
+      .appendNode(liPeakClimber)
+      .appendNode(liBallGames)
+      .appendNode(liIceAdventures);
 
     const aIceAdventures = new ElementAnchorCreator({
       href: '#',
@@ -118,46 +112,42 @@ class Header {
     liIceAdventures.appendNode(aIceAdventures);
 
     const divBtns = new ElementCreator({ tag: 'div', classes: 'flex gap-6' });
-    mobileMenu.appendNode(divBtns);
+    mobileMenu.appendNode(linksList).appendNode(divBtns);
 
     const btnSignUp = new ElementButtonCreator({
       tag: 'button',
       text: 'sign up',
       classes: 'secondary-button',
     });
-    divBtns.appendNode(btnSignUp);
 
     const btnLogIn = new ElementButtonCreator({
       tag: 'button',
       text: 'log in',
       classes: 'primary-button',
     });
-    divBtns.appendNode(btnLogIn);
+    divBtns.appendNode(btnSignUp).appendNode(btnLogIn);
 
     const burger = new ElementCreator({
       tag: 'div',
       classes: 'burger space-y-2 z-10 block md:hidden',
     });
-    nav.appendNode(burger);
 
     const spanBurger1 = new ElementCreator({
       tag: 'span',
       classes: 'block w-8 h-0.5 bg-secondary-color',
     });
-    burger.appendNode(spanBurger1);
     const spanBurger2 = new ElementCreator({
       tag: 'span',
       classes: 'block w-8 h-0.5 bg-secondary-color',
     });
-    burger.appendNode(spanBurger2);
     const spanBurger3 = new ElementCreator({
       tag: 'span',
       classes: 'block w-8 h-0.5 bg-secondary-color',
     });
-    burger.appendNode(spanBurger3);
+    burger.appendNode(spanBurger1).appendNode(spanBurger2).appendNode(spanBurger3);
 
     const bg = new ElementCreator({ tag: 'div', classes: 'bg hidden' });
-    nav.appendNode(bg);
+    nav.appendNode(logo).appendNode(mobileMenu).appendNode(burger).appendNode(bg);
 
     burger.getElement().addEventListener('click', () => {
       mobileMenu.getElement().classList.toggle('active');
