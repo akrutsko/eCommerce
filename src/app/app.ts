59c91c4b2d694d4860a5a51847b3c39cadbb0d97
Sviatlana Yurusava <88201602+Sviatl@users.noreply.github.com>
2023-08-13 20:50:34 +0300

    feat: layout/header view (#14)
    * feat: add header view component
    
    * feat: add header elements
    
    * refactor: prittier changes of header
    
    * feat: add ElementAnchorCreator in header.css and svg logo as external file
    
    * refactor: use assets imports before all others
    
    * refactor: chain in appendNode, rename ul in linkList
    
    * refactor: chain in appendNode, rename ul in linkList
    
    * feat: add header view component
    
    * feat: add header elements
    
    * refactor: prittier changes of header
    
    * feat: add ElementAnchorCreator in header.css and svg logo as external file
    
    * refactor: use assets imports before all others
    
    * refactor: chain in appendNode, rename ul in linkList
    
    * refactor: chain in appendNode, rename ul in linkList
    
    * feat: add header test
    
    * feat: add cart, customer links and sign out button
    
    * feat: add cart, customer links and sign out button
    
    * refactor: add comments TODO and change export place

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
 import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
 
 export default class App {
+  header: Header;
+
   footer: Footer;
 
   constructor() {
+    this.header = new Header();
     this.footer = new Footer();
   }
 
   init(): void {
+    document.body.append(this.header.getElement());
     document.body.append(this.footer.getElement());
   }
 }
