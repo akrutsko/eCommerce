181fa0085fe7086c4740a65b2173ab04d060b4c9
Sviatlana Yurusava <88201602+Sviatl@users.noreply.github.com>
2023-08-15 23:03:09 +0300

    feat: add a router for navigating between application views (#21)
    * feat: add class router
    
    * feat: merged with login-registration-main
    
    * feat: routing login/signup/main
    
    * refactor: change type of routes: Record<string, string>
    
    * refactor: change AS to instanceof
    
    * refactor: use foreach nstead of let i = 0...
    
    * feat: add laizy loading for routing views
    
    * feat: change main view for routing
    
    * feat: add 404 to main view and router
    
    * feat: add 404 to main view and router
    
    * refactor: remove unuser async
    
    * refactor: remove unuser async
    
    * refactor: remove unuser async
    
    * refactor: saparate list of routers
    
    * feat: add hashValue in router
    
    * feat: add hashValue in router
    
    * fix: sintax link ice-adventures
    
    * refactor: delete unused navigateToLogin
    
    * refactor: change click event on mouseover in header categories
    
    * refactor: change click event on mouseover in header categories

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Consumer } from './components/consumer/consumer';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { Main } from './components/main/main';
-import { Registration } from './components/registration/registration';
+import { Router } from './router/router';
 
 export default class App {
   header: Header;
@@ -13,17 +13,22 @@ export default class App {
 
   consumer: Consumer;
 
+  router: Router;
+
   constructor() {
+    this.router = new Router();
     this.consumer = new Consumer();
     this.header = new Header(this.consumer);
     this.main = new Main();
     this.footer = new Footer();
+
+    this.router.subscribe(this.main);
+    this.router.handleLocation();
   }
 
   init(): void {
     document.body.append(this.header.getElement());
-    document.body.append(this.main.getElement());
-    this.main.getElement().append(new Registration().getElement());
+    document.body.append(this.main.getView());
     document.body.append(this.footer.getElement());
 
     this.consumer.subscribe(this.header);
---
+++ src/app/router/router.ts
@@ -0,0 +1,33 @@
+import { routes } from './routes';
+
+export class Router implements Observable {
+  private observers: Observer[] = [];
+
+  constructor() {
+    window.onpopstate = this.handleLocation;
+  }
+
+  subscribe(observer: Observer): void {
+    if (this.observers.includes(observer)) return;
+    this.observers.push(observer);
+  }
+
+  unsubscribe(observer: Observer): void {
+    const index = this.observers.indexOf(observer);
+    if (index === -1) return;
+    this.observers.slice(index, 1);
+  }
+
+  notify(primaryData?: string, secondaryData?: string): void {
+    if (primaryData) {
+      this.observers.forEach((observer) => observer.update(primaryData, secondaryData));
+    }
+  }
+
+  public handleLocation = (): void => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const hashValue = window.location.hash;
+    this.notify(route, hashValue);
+  };
+}
