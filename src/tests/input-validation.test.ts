761f171a683fbcf736d522b11f827ec03d797c8d
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-08-21 21:38:37 +0300

    feat: add validation tests

--- src/tests/input-validation.test.ts
+++ src/tests/input-validation.test.ts
@@ -1,5 +1,21 @@
+import { isValueExist, validateEmail, validatePassword } from '../app/utils/validation/input-validation';
+
 describe('Input validation tests', () => {
   test('Value exists', () => {
+    expect(isValueExist('a')).toBeTruthy();
+    expect(isValueExist('1')).toBeTruthy();
+    expect(isValueExist('@')).toBeTruthy();
+  });
 
+  test('Email is valid', () => {
+    expect(validateEmail('t@t.by')).toBeTruthy();
+    expect(validateEmail('t@t').isValid).toBeFalsy();
+  });
+
+  test('Password is valid', () => {
+    expect(validatePassword('Secret123')).toBeTruthy();
+    expect(validatePassword('aAaaaaaa1')).toBeTruthy();
+    expect(validatePassword('BBBBBBBB2b')).toBeTruthy();
+    expect(validatePassword('secret123').isValid).toBeFalsy();
   });
 });
