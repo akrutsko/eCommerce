647c0f0f2f6dc9e35b39b44db2207752777b7ee2
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-08-24 10:35:32 +0300

    feat: add first string of catalog

--- src/app/components/catalog/catalog.ts
+++ src/app/components/catalog/catalog.ts
@@ -1,21 +1,35 @@
+import searchIcon from '../../../assets/svg/search.svg';
+import { ElementButtonCreator } from '../../utils/element-creator/element-button-creator';
 import { ElementCreator } from '../../utils/element-creator/element-creator';
+import { ElementInputCreator } from '../../utils/element-creator/element-input-creator';
 
 export class Catalog {
   catalogView: ElementCreator<HTMLElement>;
 
   constructor() {
-    this.catalogView = new ElementCreator({ tag: 'div' });
+    this.catalogView = new ElementCreator({ tag: 'div', classes: 'w-full items-top' });
     this.createView();
   }
 
   createView(): void {
-    const mainMessage = new ElementCreator({
-      tag: 'div',
-      classes: 'text-[#DFDDDF] text-8xl sd:text-[265px] mg:text-[350px] font-bold drop-shadow-[5px_4px_0px_rgba(57,62,77,0.18)]',
-      text: 'Catalog',
-    });
+    const firstBlock = new ElementCreator({ tag: 'div', classes: 'w-full items-top justify-between flex gap-1' });
+    const catalogNameBlock = new ElementCreator({ tag: 'div', classes: '' });
+    const breadcrumbsBlock = new ElementCreator({ tag: 'div', text: '1>2', classes: 'breadcrumbs' });
+    const catalogName = new ElementCreator({ tag: 'h2', text: 'Catalog', classes: 'h2' });
+    catalogNameBlock.appendNode(catalogName, breadcrumbsBlock);
 
-    this.catalogView.appendNode(mainMessage);
+    const form = new ElementCreator({ tag: 'form', classes: 'search-form' });
+    const search = new ElementInputCreator({ type: 'search', name: 'search', placeholder: 'search' });
+    search.getElement().addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+      }
+    }); // TODO: implement search
+    const submitButton = new ElementButtonCreator({ classes: 'absolute right-0 top-0 focus:outline-none', html: searchIcon });
+    form.appendNode(search, submitButton);
+    firstBlock.appendNode(catalogNameBlock, form);
+
+    this.catalogView.appendNode(firstBlock);
   }
 
   getView(): ElementCreator<HTMLElement> {
