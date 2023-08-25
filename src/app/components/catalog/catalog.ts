b4f237f103339995af51b2b5464d43d1e7e68b0b
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-08-25 11:28:39 +0300

    feat: add images to cards

--- src/app/components/catalog/catalog.ts
+++ src/app/components/catalog/catalog.ts
@@ -1,15 +1,24 @@
+import { ProductProjection } from '@commercetools/platform-sdk';
 import searchIcon from '../../../assets/svg/search.svg';
 import './catalog.css';
 import { ElementButtonCreator } from '../../utils/element-creator/element-button-creator';
 import { ElementCreator } from '../../utils/element-creator/element-creator';
 import { ElementInputCreator } from '../../utils/element-creator/element-input-creator';
+import { getProductProjections } from '../../utils/api/api-product-projections';
+import { getCtpClient } from '../../utils/api/api-client';
 
 export class Catalog {
   catalogView: ElementCreator<HTMLElement>;
 
   cardsList: ElementCreator<HTMLElement>[];
 
+  cardsElementCreator: ElementCreator<HTMLElement>;
+
   constructor() {
+    this.cardsElementCreator = new ElementCreator({
+      tag: 'div',
+      classes: 'w-full md:w-2/4 lg:w-6/8 flex flex-wrap gap-3 justify-around grow',
+    });
     this.cardsList = [];
     this.catalogView = new ElementCreator({ tag: 'div', classes: 'w-full grow flex flex-col items-top' });
     this.createView();
@@ -47,23 +56,54 @@ export class Catalog {
       classes: 'w-full md:w-1/4 lg:w-1/8 flex flex-col flex-wrap border border-1 border-blue-500 filters',
       text: 'filters',
     });
-    const cards = new ElementCreator({
-      tag: 'div',
-      classes: 'w-full md:w-2/4 lg:w-6/8 flex flex-wrap gap-3 justify-around grow',
-    });
-    thirdBlock.appendNode(filters, cards);
 
-    for (let i = 0; i < 9; i += 1) {
-      const card = new ElementCreator({
-        tag: 'div',
-        classes: 'card w-60 h-72 border border-1 border-blue-500',
-        text: `card ${i}`,
-      });
-      cards.appendNode(card);
-      this.cardsList.push(card);
-    }
+    thirdBlock.appendNode(filters, this.cardsElementCreator);
+
+    this.createCards();
 
     this.catalogView.appendNode(firstBlock, secondBlock, thirdBlock);
+  }
+
+  async createCards(): Promise<void> {
+    const productsResponse = await getProductProjections(getCtpClient());
+    console.log(productsResponse);
+    if (productsResponse.statusCode === 200) {
+      const { results } = productsResponse.body;
+      results.forEach((product) => {
+        this.addProduct(product);
+      });
+    }
+  }
+
+  addProduct(product: ProductProjection): void {
+    const localizedString = 'en-US';
+    const productName = product.name[localizedString];
+    const card = new ElementCreator({
+      tag: 'div',
+      classes: 'card w-60 h-72 border border-1 border-blue-500',
+      text: `${productName}`,
+    });
+    this.cardsElementCreator.appendNode(card);
+    this.cardsList.push(card);
+
+    const rectangle = document.createElement('div');
+    rectangle.classList.add('w-60', 'h-60', 'rounded-10', 'border-2', 'p-4');
+
+    const image = document.createElement('img');
+    const { variants } = product;
+    if (variants) {
+      const { images } = variants[0];
+      if (images) {
+        const { url } = images[0];
+        image.src = url;
+      }
+    }
+    image.alt = 'Изображение';
+    image.classList.add('w-full', 'h-full', 'object-cover');
+
+    rectangle.appendChild(image);
+
+    card.getElement().append(rectangle);
   }
 
   getView(): ElementCreator<HTMLElement> {
---
+++ src/app/utils/api/api-product-projections.ts
@@ -0,0 +1,83 @@
+import { Client } from '@commercetools/sdk-client-v2';
+import { ClientResponse, ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
+import { getApiRoot } from './api-client';
+
+export function getProductProjections(client: Client): Promise<ClientResponse<ProductProjectionPagedQueryResponse>> {
+  return getApiRoot(client).productProjections().get({ queryArgs: { staged: true, limit: 9, offset: 0 } }).execute();
+  // return [
+  //   {
+  //     method: 'get',
+  //     uri: '/test_projectKey/product-projections/suggest?fuzzy=true',
+  //     request: apiRoot
+  //       .withProjectKey({ projectKey: 'test_projectKey' })
+  //       .productProjections()
+  //       .suggest()
+  //       .get({ queryArgs: { fuzzy: true } }),
+  //   },
+  //   {
+  //     method: 'get',
+  //     uri: '/test_projectKey/product-projections/suggest?searchKeywords.locale=searchKeywords.locale',
+  //     request: apiRoot
+  //       .withProjectKey({ projectKey: 'test_projectKey' })
+  //       .productProjections()
+  //       .suggest()
+  //       .get({
+  //         queryArgs: { 'searchKeywords.locale': 'searchKeywords.locale' },
+  //       }),
+  //   },
+  //   {
+  //     method: 'get',
+  //     uri: '/test_projectKey/product-projections/suggest?sort=sort',
+  //     request: apiRoot
+  //       .withProjectKey({ projectKey: 'test_projectKey' })
+  //       .productProjections()
+  //       .suggest()
+  //       .get({ queryArgs: { sort: 'sort' } }),
+  //   },
+  //   {
+  //     method: 'get',
+  //     uri: '/test_projectKey/product-projections/suggest?limit=7',
+  //     request: apiRoot
+  //       .withProjectKey({ projectKey: 'test_projectKey' })
+  //       .productProjections()
+  //       .suggest()
+  //       .get({ queryArgs: { limit: 7 } }),
+  //   },
+  //   {
+  //     method: 'get',
+  //     uri: '/test_projectKey/product-projections/suggest?offset=3',
+  //     request: apiRoot
+  //       .withProjectKey({ projectKey: 'test_projectKey' })
+  //       .productProjections()
+  //       .suggest()
+  //       .get({ queryArgs: { offset: 3 } }),
+  //   },
+  //   {
+  //     method: 'get',
+  //     uri: '/test_projectKey/product-projections/suggest?withTotal=true',
+  //     request: apiRoot
+  //       .withProjectKey({ projectKey: 'test_projectKey' })
+  //       .productProjections()
+  //       .suggest()
+  //       .get({ queryArgs: { withTotal: true } }),
+  //   },
+  //   {
+  //     method: 'get',
+  //     uri: '/test_projectKey/product-projections/suggest?staged=true',
+  //     request: apiRoot
+  //       .withProjectKey({ projectKey: 'test_projectKey' })
+  //       .productProjections()
+  //       .suggest()
+  //       .get({ queryArgs: { staged: true } }),
+  //   },
+  //   {
+  //     method: 'get',
+  //     uri: '/test_projectKey/product-projections/suggest',
+  //     request: apiRoot
+  //       .withProjectKey({ projectKey: 'test_projectKey' })
+  //       .productProjections()
+  //       .suggest()
+  //       .get(),
+  //   },
+  // ];
+}
