2aea7452f73542cd7f6bea32bd026e478188d884
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-08-27 19:19:46 +0300

    feat: implement swiper slider

--- src/app/components/product/product.ts
+++ src/app/components/product/product.ts
@@ -1,7 +1,11 @@
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import './product.css';
 
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import { ProductProjection } from '@commercetools/platform-sdk';
 import { ElementCreator } from '../../utils/element-creator/element-creator';
 import { HandlerLinks } from '../../router/handler-links';
@@ -16,9 +20,11 @@ export class Product extends HandlerLinks {
 
   productId: string;
 
-  productData: ProductProjection | undefined;
-
   productView: ElementCreator<HTMLElement>;
+
+  productImage: HTMLImageElement | undefined;
+
+  productData: ProductProjection | undefined;
 
   swiper: Swiper | undefined;
 
@@ -41,64 +47,16 @@ export class Product extends HandlerLinks {
 
     const name = this.productData.name['en-US'];
     const description = this.productData.description?.['en-US'] || '';
-    const images = this.productData.masterVariant.images?.map((image) => image.url);
-    const mainImgUrl = images?.[0] || '';
-
-    console.log(this.productData);
+    const mainImgUrl = this.productData.masterVariant.images?.[0].url || '';
 
     const breadcrumbWrapper = new ElementCreator({ tag: 'div', classes: 'mb-2.5' }); // TODO: generate breadcrumb
 
     const imageWrapper = new ElementCreator({
       tag: 'div',
-      classes: 'max-w-sm w-72 aspect-square bg-white rounded-xl',
+      classes: 'max-w-sm w-72 aspect-square bg-white rounded-xl cursor-zoom-in',
     });
-    const productImage = new ElementImageCreator({ src: mainImgUrl, alt: name, classes: 'h-full object-cover' });
-    imageWrapper.appendNode(productImage);
-
-    const swiperEl = new ElementCreator({
-      tag: 'div',
-      classes: 'swiper w-[212px] md:w-16 h-16 md:h-[212px] shrink-0 flex md:flex-col md:order-first select-none',
-    });
-    const swiperWrapper = new ElementCreator({ tag: 'div', classes: 'swiper-wrapper' });
-    images?.forEach((image) => {
-      const imgWrapper = new ElementCreator({
-        tag: 'div',
-        classes: 'swiper-slide aspect-square bg-white rounded-md object-cover',
-      });
-      const sliderImage = new ElementImageCreator({ src: image, alt: '', classes: 'h-full object-cover' });
-      imgWrapper.appendNode(sliderImage);
-      swiperWrapper.appendNode(imgWrapper);
-    });
-    swiperEl.appendNode(swiperWrapper);
-    const swiperPrevButton = new ElementCreator({ tag: 'div', classes: 'swiper-button-prev' });
-    const swiperNextButton = new ElementCreator({ tag: 'div', classes: 'swiper-button-next' });
-    swiperEl.appendNode(swiperPrevButton, swiperNextButton);
-
-    this.swiper = new Swiper(swiperEl.getElement(), {
-      enabled: Boolean(images?.length && images?.length > 1),
-      slidesPerView: 3,
-      centerInsufficientSlides: true,
-      spaceBetween: 10,
-      direction: window.innerWidth < 768 ? 'horizontal' : 'vertical',
-      loop: true,
-      grabCursor: true,
-      navigation: {
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next',
-      },
-    });
-
-    this.swiper.on('click', (swiper) => {
-      if (images) {
-        productImage.getElement().src = images[swiper.clickedIndex];
-      }
-    });
-    this.swiper.on('resize', (swiper) => {
-      swiper.changeDirection(window.innerWidth < 768 ? 'horizontal' : 'vertical');
-    });
-
-    swiperPrevButton.setHandler('click', () => this.swiper?.slidePrev());
-    swiperNextButton.setHandler('click', () => this.swiper?.slideNext());
+    this.productImage = new ElementImageCreator({ src: mainImgUrl, alt: name, classes: 'h-full object-cover' }).getElement();
+    imageWrapper.appendNode(this.productImage);
 
     const productWrapper = new ElementCreator({ tag: 'div', classes: 'self-start grow' });
     const productName = new ElementCreator({ tag: 'h3', text: name });
@@ -106,9 +64,68 @@ export class Product extends HandlerLinks {
     productWrapper.appendNode(productName, productDescription);
 
     const wrapper = new ElementCreator({ tag: 'div', classes: 'flex flex-col items-center gap-2.5 md:flex-row md:gap-14' });
-    wrapper.appendNode(imageWrapper, swiperEl, productWrapper);
+    const swiper = new ElementCreator({ tag: 'div', classes: 'shrink-0 flex md:flex-col md:order-first select-none relative' });
+    wrapper.appendNode(imageWrapper, swiper, productWrapper);
 
     this.productView.appendNode(breadcrumbWrapper, wrapper);
+
+    this.initSwiper(swiper.getElement());
+  }
+
+  initSwiper(wrapper: HTMLElement): void {
+    if (!this.productData || !this.productData.masterVariant.images) return;
+
+    const images = this.productData.masterVariant.images.map((image) => image.url);
+
+    const swiperContainer = new ElementCreator({
+      tag: 'div',
+      classes: 'swiper w-[212px] md:w-16 h-16 md:h-[212px]',
+    });
+    const swiperWrapper = new ElementCreator({ tag: 'div', classes: 'swiper-wrapper' });
+    images.forEach((image) => {
+      const imgWrapper = new ElementCreator({
+        tag: 'div',
+        classes: 'swiper-slide aspect-square bg-white rounded-md object-cover cursor-pointer',
+      });
+      const sliderImage = new ElementImageCreator({ src: image, alt: '', classes: 'h-full object-cover' });
+      imgWrapper.appendNode(sliderImage);
+      swiperWrapper.appendNode(imgWrapper);
+    });
+    swiperContainer.appendNode(swiperWrapper);
+
+    const swiperPrevButton = new ElementCreator({ tag: 'div', classes: 'swiper-button-prev' });
+    const swiperNextButton = new ElementCreator({ tag: 'div', classes: 'swiper-button-next' });
+    wrapper.append(swiperContainer.getElement(), swiperPrevButton.getElement(), swiperNextButton.getElement());
+
+    const swiperNavigation = new ElementCreator({ tag: 'div', classes: 'swiper-pagination hidden md:block' });
+    wrapper.append(swiperNavigation.getElement());
+
+    const swiper = new Swiper('.swiper', {
+      enabled: images.length > 1,
+      modules: [Navigation, Pagination],
+      slidesPerView: 3,
+      centerInsufficientSlides: true,
+      spaceBetween: 10,
+      direction: window.innerWidth < 768 ? 'horizontal' : 'vertical',
+      loop: true,
+      navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+    swiper.on('click', () => {
+      if (this.productImage && swiper.clickedSlide && swiper.clickedSlide.dataset.swiperSlideIndex) {
+        this.productImage.src = images[+swiper.clickedSlide.dataset.swiperSlideIndex];
+      }
+    });
+    swiper.on('resize', () => {
+      swiper.changeDirection(window.innerWidth < 768 ? 'horizontal' : 'vertical');
+    });
   }
 
   getView(): ElementCreator<HTMLElement> {
