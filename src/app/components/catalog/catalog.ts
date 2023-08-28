9d2430a8540afed6955c9287081e8d8571ab1f11
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-08-28 10:36:07 +0300

    feat: add price color

--- src/app/components/catalog/catalog.ts
+++ src/app/components/catalog/catalog.ts
@@ -1,4 +1,4 @@
-import { ProductProjection } from '@commercetools/platform-sdk';
+import { ProductProjection, TypedMoney } from '@commercetools/platform-sdk';
 import searchIcon from '../../../assets/svg/search.svg';
 import './catalog.css';
 import { ElementButtonCreator } from '../../utils/element-creator/element-button-creator';
@@ -7,6 +7,7 @@ import { ElementInputCreator } from '../../utils/element-creator/element-input-c
 import { getProductProjections } from '../../utils/api/api-product-projections';
 import { getCtpClient } from '../../utils/api/api-client';
 import { ElementImageCreator } from '../../utils/element-creator/element-image-creator';
+import { Message } from '../../utils/message/toastify-message';
 
 export class Catalog {
   catalogView: ElementCreator<HTMLElement>;
@@ -47,14 +48,14 @@ export class Catalog {
     firstBlock.appendNode(catalogNameBlock, form);
 
     const secondBlock = new ElementCreator({ tag: 'div', classes: 'w-full items-top justify-between flex gap-1' });
-    const seletedFilfers = new ElementCreator({ tag: 'div', classes: '', text: 'filters' });
+    const selectedFilfers = new ElementCreator({ tag: 'div', classes: '', text: 'filters' });
     const countOfResults = new ElementCreator({ tag: 'div', classes: '', text: '12 results' });
-    secondBlock.appendNode(seletedFilfers, countOfResults);
+    secondBlock.appendNode(selectedFilfers, countOfResults);
 
     const thirdBlock = new ElementCreator({ tag: 'div', classes: 'w-full justify-between flex gap-3 flex-wrap' });
     const filters = new ElementCreator({
       tag: 'div',
-      classes: 'w-full md:w-1/4 lg:w-1/8 flex flex-col flex-wrap border border-1 border-blue-500 filters',
+      classes: 'w-full md:w-1/4 lg:w-1/8 flex flex-col flex-wrap border border-1 border-blue-500 filters flex-grow-0 flex-shrink-0',
       text: 'filters',
     });
 
@@ -66,13 +67,29 @@ export class Catalog {
   }
 
   async createCards(): Promise<void> {
-    const productsResponse = await getProductProjections(getCtpClient());
-    if (productsResponse.statusCode === 200) {
-      const { results } = productsResponse.body;
-      results.forEach((product) => {
-        this.addProduct(product);
-      });
+    try {
+      const productsResponse = await getProductProjections(getCtpClient());
+      if (productsResponse.statusCode === 200) {
+        const { results } = productsResponse.body;
+        results.forEach((product) => {
+          this.addProduct(product);
+        });
+      } else {
+        new Message('Something went wrong. Try later.', 'error').showMessage();
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        if (err.message) {
+          new Message(err.message, 'error').showMessage();
+        } else {
+          new Message('Something went wrong. Try later.', 'error').showMessage();
+        }
+      }
     }
+  }
+
+  formatPrice(price: TypedMoney): string {
+    return `$${(price.centAmount / (100 * price.fractionDigits)).toFixed(2)}`;
   }
 
   addProduct(product: ProductProjection): void {
@@ -105,11 +122,9 @@ export class Catalog {
       }
       const { prices } = masterVariant;
       if (prices?.length) {
-        priceWithOutDiscount = `${prices[0].value.centAmount / prices[0].value.fractionDigits}${prices[0].value.currencyCode}`;
+        priceWithOutDiscount = this.formatPrice(prices[0].value);
         if (prices[0].discounted && prices[0].discounted.value) {
-          price = `${prices[0].discounted.value.centAmount / prices[0].discounted.value.fractionDigits}${
-            prices[0].discounted.value.currencyCode
-          }`;
+          price = this.formatPrice(prices[0].discounted.value);
         } else {
           price = priceWithOutDiscount;
         }
@@ -127,13 +142,13 @@ export class Catalog {
         'font-open-sans text-xs font-normal leading-4 tracking-normal h-8 overflow-hidden whitespace-normal overflow-ellipsis',
     });
 
-    const productPricesBlock = new ElementCreator({ tag: 'div', classes: 'flex' });
+    const productPricesBlock = new ElementCreator({ tag: 'div', classes: 'flex gap-2' });
 
-    const productPriceBlock = new ElementCreator({ tag: 'div', text: `${price}` });
+    const productPriceBlock = new ElementCreator({ tag: 'div', text: `${price}`, classes: 'font-sans text-xl font-semibold leading-6 tracking-wider text-[#DB5743]' });
     const productPriceWithOutDiscountBlock = new ElementCreator({
       tag: 'div',
       text: `${priceWithOutDiscount}`,
-      classes: 'line-through',
+      classes: 'line-through font-sans text-xl font-semibold leading-6 tracking-wider text-[#f9b8b3]',
     });
 
     productPricesBlock.appendNode(productPriceBlock);
