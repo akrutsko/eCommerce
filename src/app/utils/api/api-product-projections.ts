d3cc8d7aeb18d1348293b36c37a3d9dec5d5ad7b
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-08-29 11:38:51 +0300

    feat: add filter by category

--- src/app/utils/api/api-product-projections.ts
+++ src/app/utils/api/api-product-projections.ts
@@ -2,10 +2,13 @@ import { Client } from '@commercetools/sdk-client-v2';
 import { ClientResponse, ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
 import { getApiRoot } from './api-client';
 
-export function getProductProjections(client: Client, filter?: string)
-  : Promise<ClientResponse<ProductProjectionPagedQueryResponse>> {
+export function getProductProjections(
+  client: Client,
+  filter?: string,
+): Promise<ClientResponse<ProductProjectionPagedQueryResponse>> {
   return getApiRoot(client)
-    .productProjections().search()
+    .productProjections()
+    .search()
     .get({ queryArgs: { limit: 30, offset: 0, filter } })
     .execute();
 }
