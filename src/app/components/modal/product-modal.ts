36ad8e3e18071c61a0a685a41732a02ea426de7d
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-08-31 11:48:51 +0300

    feat: implement enlarged image modal with slider (#34)

---
+++ src/app/components/modal/product-modal.ts
@@ -0,0 +1,57 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './product-modal.css';
+
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import { ProductProjection } from '@commercetools/platform-sdk';
+import { ElementCreator } from '../../utils/element-creator/element-creator';
+import { ElementImageCreator } from '../../utils/element-creator/element-image-creator';
+import { Modal } from './modal';
+
+export class ProductModal extends Modal {
+  productData: ProductProjection;
+
+  swiper: Swiper | undefined;
+
+  constructor(productData: ProductProjection) {
+    super();
+    this.productData = productData;
+
+    this.setView();
+  }
+
+  setView(): void {
+    if (!this.productData.masterVariant.images) return;
+
+    const images = this.productData.masterVariant.images.map((image) => image.url);
+
+    const swiperContainer = new ElementCreator({ tag: 'div', classes: 'swiper swiper-modal max-w-5xl' });
+    const swiperWrapper = new ElementCreator({ tag: 'div', classes: 'swiper-wrapper items-center' });
+    images.forEach((image) => {
+      const imgWrapper = new ElementCreator({ tag: 'div', classes: 'swiper-slide swiper-slide-modal bg-white' });
+      const sliderImage = new ElementImageCreator({ src: image, alt: '', classes: 'object-cover' });
+      imgWrapper.appendNode(sliderImage);
+      swiperWrapper.appendNode(imgWrapper);
+    });
+    swiperContainer.appendNode(swiperWrapper);
+
+    const swiperPrevButton = new ElementCreator({ tag: 'div', classes: 'swiper-button-prev swiper-button-prev-modal' });
+    const swiperNextButton = new ElementCreator({ tag: 'div', classes: 'swiper-button-next swiper-button-next-modal' });
+    this.getView().appendNode(swiperContainer, swiperPrevButton, swiperNextButton);
+
+    this.swiper = new Swiper('.swiper-modal', {
+      modules: [Navigation],
+      slidesPerView: 1,
+      direction: 'horizontal',
+      loop: true,
+      navigation: { prevEl: '.swiper-button-prev-modal', nextEl: '.swiper-button-next-modal' },
+    });
+  }
+
+  initSwiper(index = 0): void {
+    if (!this.swiper) return;
+    this.swiper.init();
+    this.swiper.slideTo(index, 0);
+  }
+}
--- src/tests/api-product.test.ts
+++ src/tests/api-product.test.ts
@@ -1,6 +1,6 @@
 import 'jest-fetch-mock';
 import { getCtpClient } from '../app/utils/api/api-client';
-import { getProduct, getProductProjection } from '../app/utils/api/api-product';
+import { getProduct, getProductIdBySlug, getProductProjection } from '../app/utils/api/api-product';
 
 describe('Tests for product API', () => {
   test('Get a product', async () => {
@@ -13,5 +13,11 @@ describe('Tests for product API', () => {
     const productWithId = await getProductProjection(getCtpClient(), 'c43011cc-de74-406f-b68e-02c0441bcdb1');
 
     expect(productWithId.statusCode).toBe(200);
+  });
+
+  test('Get the product by slug', async () => {
+    const productId = await getProductIdBySlug(getCtpClient(), 'cozytrek-camping-tent');
+
+    expect(productId.body.results[0].id).toBe('c43011cc-de74-406f-b68e-02c0441bcdb1');
   });
 });
