d620331eeb2f35d66908028190b382caa53bc511
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-08-31 11:48:51 +0300

    feat: implement enlarged image modal with slider (#34)

---
+++ src/app/components/modal/modal.ts
@@ -0,0 +1,38 @@
+import { ElementButtonCreator } from '../../utils/element-creator/element-button-creator';
+import { ElementCreator } from '../../utils/element-creator/element-creator';
+import { ElementDialogCreator } from '../../utils/element-creator/element-dialog-creator';
+
+export class Modal {
+  modalView: ElementCreator<HTMLDialogElement>;
+
+  constructor() {
+    this.modalView = new ElementDialogCreator({ tag: 'dialog', classes: 'fixed select-none' });
+
+    this.createView();
+  }
+
+  createView(): void {
+    const closeButton = new ElementButtonCreator({
+      tag: 'button',
+      classes: 'absolute z-10 top-5 right-5 scale-150 cursor-pointer outline-none text-primary-color',
+      text: '✖',
+    });
+    closeButton.setHandler('click', () => {
+      this.getElement().close();
+      this.getElement().remove();
+    });
+    this.modalView.appendNode(closeButton);
+  }
+
+  getView(): ElementCreator<HTMLDialogElement> {
+    return this.modalView;
+  }
+
+  getElement(): HTMLDialogElement {
+    return this.modalView.getElement();
+  }
+
+  showModal(): void {
+    this.getElement().showModal();
+  }
+}
