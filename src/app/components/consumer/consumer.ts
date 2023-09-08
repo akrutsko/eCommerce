50c894e7e462b68813546b5c90034bc3c3b6d672
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-09-08 22:28:45 +0300

    feat: fetch and display cart items, display empty cart

--- src/app/components/consumer/consumer.ts
+++ src/app/components/consumer/consumer.ts
@@ -25,7 +25,7 @@ import {
   setDefaultShippingAddress,
 } from '../../utils/api/api-consumer';
 import { Token } from '../../enums/token';
-import { createCart, getActiveCart } from '../../utils/api/api-cart';
+import { addToCart, createCart, getActiveCart, updateQuantity } from '../../utils/api/api-cart';
 
 export class Consumer implements Observable {
   observers: Observer[] = [];
@@ -96,6 +96,20 @@ export class Consumer implements Observable {
     } else {
       this.cart = (await createCart(this.apiClient, { currency: 'USD' })).body;
     }
+
+    // TODO: remove
+    this.cart = (await addToCart(this.apiClient, this.cart.version, this.cart.id, '8ef892fb-cd1f-47e1-8a7f-c38c0ac57f27')).body;
+    const lineItemId = this.cart.lineItems.filter((lineItem) => lineItem.productId === '8ef892fb-cd1f-47e1-8a7f-c38c0ac57f27')[0]
+      .id;
+    this.cart = (await updateQuantity(this.apiClient, this.cart.version, this.cart.id, lineItemId, 2)).body;
+    this.cart = (await addToCart(this.apiClient, this.cart.version, this.cart.id, 'a632302c-d91d-499b-b680-6d29a1f22c19')).body;
+    this.cart = (await addToCart(this.apiClient, this.cart.version, this.cart.id, '4dab7e57-080a-43a3-9fb3-a55d95b91c9d')).body;
+    this.cart = (await addToCart(this.apiClient, this.cart.version, this.cart.id, '19328891-6cc1-46b2-9618-fa4404a06f24')).body;
+    this.cart = (await addToCart(this.apiClient, this.cart.version, this.cart.id, '635e5297-6c5f-412b-831c-35d72f5f6c15')).body;
+    this.cart = (await addToCart(this.apiClient, this.cart.version, this.cart.id, '8be62abc-213e-4907-85d0-8cb43c59d6da')).body;
+    this.cart = (await addToCart(this.apiClient, this.cart.version, this.cart.id, '9715bf15-891c-497a-9135-efb2437f43f0')).body;
+    this.cart = (await addToCart(this.apiClient, this.cart.version, this.cart.id, 'c57f7614-3912-4eaf-83c2-184d26b2df0b')).body;
+    // TODO: remove
 
     this.notify();
   }
