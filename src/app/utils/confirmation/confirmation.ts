17b9f8bbacd6451b079bb305bb4864cccc182afe
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-09-11 12:34:53 +0300

    feat: add removing from cart (#49)

--- src/app/utils/confirmation/confirmation.ts
+++ src/app/utils/confirmation/confirmation.ts
@@ -8,20 +8,21 @@ export class Confirmation {
     this.callback = callback;
   }
 
-  showConfirmation(text: string): void {
+  showConfirmation(text?: string): void {
     const bg = new ElementCreator({ tag: 'div', classes: 'absolute bg-secondary-color opacity-50 inset-0' });
     const confirmation = new ElementCreator({
       tag: 'div',
-      classes: 'bg-[#F1EFEF] p-4 w-full absolute top-20 left-2/4 -translate-x-2/4 drop-shadow-md max-w-xs rounded-xl',
+      classes:
+        'bg-[#F1EFEF] p-4 w-full absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 drop-shadow-md max-w-xs rounded-xl',
     });
 
-    const title = new ElementCreator({ tag: 'h4', classes: 'h4 text-center', text: 'Are you shure?' });
-    const message = new ElementCreator({ tag: 'div', classes: 'text-xs opacity-60 text-center', text });
+    const title = new ElementCreator({ tag: 'h4', classes: 'h4 text-center mb-2', text: 'Are you sure?' });
+    const message = new ElementCreator({ tag: 'div', classes: 'text-xs opacity-60 text-justify mb-5', text: text || '' });
 
     const buttons = new ElementCreator({ tag: 'div', classes: 'flex gap-2 mt-3' });
     const cancelButton = new ElementButtonCreator({ classes: 'secondary-button w-full', text: 'cancel' });
     const okButton = new ElementButtonCreator({ classes: 'primary-button w-full', text: 'ok' });
-    buttons.appendNode(cancelButton, okButton);
+    buttons.appendNode(okButton, cancelButton);
 
     confirmation.appendNode(title, message, buttons);
 
@@ -29,10 +30,10 @@ export class Confirmation {
       bg.getElement().remove();
       confirmation.getElement().remove();
     });
-    okButton.setHandler('click', () => {
+    okButton.setHandler('click', async () => {
       bg.getElement().remove();
       confirmation.getElement().remove();
-      this.callback();
+      await this.callback();
     });
 
     document.body.append(bg.getElement(), confirmation.getElement());
