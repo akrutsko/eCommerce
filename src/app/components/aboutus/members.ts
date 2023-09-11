14ce267acf2caa61d619370fc45d12d1907c15b4
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-09-11 11:05:57 +0200

    feat: implement layout about us

---
+++ src/app/components/aboutus/members.ts
@@ -0,0 +1,11 @@
+import tab1 from '../../../assets/img/sveta2.png';
+import tab2 from '../../../assets/img/aliaksei2.png';
+import tab3 from '../../../assets/img/svetik2.png';
+
+import { Member } from '../../interfaces/member';
+
+export const membersList: Member[] = [
+  { name: 'sveta', img: tab1, contribution: '1' },
+  { name: 'aliaksei', img: tab2, contribution: '2' },
+  { name: 'svetik', img: tab3, contribution: '3' },
+];
