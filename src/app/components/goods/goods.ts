750d4ea255108d5f28647807479d8fcd17094a89
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-09-18 11:20:13 +0300

    fix: show card after image loading

--- src/app/components/goods/goods.ts
+++ src/app/components/goods/goods.ts
@@ -11,7 +11,8 @@ import { Store } from '../../enums/store';
 import { getPrice } from '../../utils/price/price';
 import { ElementAnchorCreator } from '../../utils/element-creator/element-anchor-creator';
 import { ElementImageCreator } from '../../utils/element-creator/element-image-creator';
-import { addToCart, createCart } from '../../utils/api/api-cart';
+import { addToMyCart, createMyCart } from '../../utils/api/api-cart';
+import { Loader } from '../loader/loader';
 
 export class Goods {
   goodsView: ElementCreator<HTMLElement>;
@@ -29,6 +30,25 @@ export class Goods {
     this.createView();
   }
 
+  async createView(): Promise<void> {
+    await this.setProductsAmount();
+    const title = new ElementCreator({
+      tag: 'h2',
+      classes: 'text-center',
+      text: "Don't know what to buy or give an athlete? Leave it to fate to decide.",
+    });
+    this.content.appendNode(title);
+
+    const button = new ElementButtonCreator({ classes: 'primary-button px-10 text-xl', text: 'push your luck' });
+    button.setHandler('click', async () => {
+      button.getElement().disabled = true;
+      await this.createCard();
+      button.getElement().disabled = false;
+    });
+
+    this.goodsView.appendNode(this.content, button);
+  }
+
   async setProductsAmount(): Promise<void> {
     const response = await getProductProjections(this.consumer.apiClient, 1, 0).catch(() => {
       new Message('Something went wrong. Try later.', 'error').showMessage();
@@ -38,34 +58,17 @@ export class Goods {
     }
   }
 
-  async createView(): Promise<void> {
-    await this.setProductsAmount();
-    const title = new ElementCreator({
-      tag: 'h2',
-      classes: 'text-center',
-      text: "Don't know what to buy or give an athlete? Leave it to fate to decide",
-    });
-    this.content.appendNode(title);
-
-    const button = new ElementButtonCreator({ classes: 'primary-button px-10 text-xl', text: 'push your luck' });
-    button.setHandler('click', () => this.createCard());
-
-    this.goodsView.appendNode(this.content, button);
-  }
-
   generateRandom(): number {
     const max = this.total || 0;
-    return Math.floor(Math.random() * (max + 1));
+    return Math.floor(Math.random() * max);
   }
 
   addProduct(product: ProductProjection): void {
     const productName = product.name[Store.Language];
-    let productDescription = '';
-    let price = '';
-    let priceWithOutDiscount = '';
-    if (product.description) {
-      productDescription = product.description[Store.Language];
-    }
+    const productDescription = product.description?.[Store.Language] || '';
+    const mainImgUrl = product.masterVariant.images?.[0].url || '';
+    const embeddedPrice = product.masterVariant.prices?.[0];
+
     const card = new ElementCreator({
       tag: 'div',
       classes:
@@ -76,32 +79,9 @@ export class Goods {
       tag: 'div',
       classes: 'relative w-full h-auto border-2 rounded-lg border-solid border-[#fbedec] p-4 bg-bg-color',
     });
-    let url = '';
-    let { masterVariant } = product;
-    const { variants } = product;
-    if (!masterVariant.isMatchingVariant) {
-      if (variants.length) {
-        const newMatchingVariant = variants.find((variant) => variant.isMatchingVariant);
-        if (newMatchingVariant) {
-          masterVariant = newMatchingVariant;
-        }
-      }
-    }
-    const { images } = masterVariant;
-    if (images) {
-      url = images[0].url;
-    }
-    const { prices } = masterVariant;
-    if (prices?.length) {
-      priceWithOutDiscount = getPrice(prices[0].value);
-      if (prices[0].discounted && prices[0].discounted.value) {
-        price = getPrice(prices[0].discounted.value);
-      } else {
-        price = priceWithOutDiscount;
-      }
-    }
+
     const aCard = new ElementAnchorCreator({ href: `/product/${product.slug[Store.Language]}`, classes: 'absolute inset-0' });
-    const image = new ElementImageCreator({ alt: productName, src: url, classes: 'w-full h-full object-cover' });
+    const image = new ElementImageCreator({ alt: productName, src: mainImgUrl, classes: 'w-full h-full object-cover' });
     productImageBlock.appendNode(image, aCard);
 
     const infoBlock = new ElementCreator({ tag: 'div', classes: 'p-3 h-full flex flex-col justify-between gap-1' });
@@ -118,14 +98,25 @@ export class Goods {
 
     const productPricesBlock = new ElementCreator({ tag: 'div', classes: 'flex gap-2 items-center' });
 
+    let priceWithOutDiscount = '';
+    let price = '';
+    if (embeddedPrice) {
+      priceWithOutDiscount = getPrice(embeddedPrice.value);
+      if (embeddedPrice.discounted?.value) {
+        price = getPrice(embeddedPrice.discounted.value);
+      } else {
+        price = priceWithOutDiscount;
+      }
+    }
+
     const productPriceBlock = new ElementCreator({
       tag: 'div',
-      text: `${price}`,
+      text: price,
       classes: 'font-sans text-xl font-semibold leading-6 tracking-wider text-[#DB5743] self-end',
     });
     const productPriceWithOutDiscountBlock = new ElementCreator({
       tag: 'div',
-      text: `${priceWithOutDiscount}`,
+      text: priceWithOutDiscount,
       classes: 'subtitle line-through',
     });
 
@@ -134,26 +125,26 @@ export class Goods {
       productPricesBlock.appendNode(productPriceWithOutDiscountBlock);
     }
 
-    const addButton = new ElementButtonCreator({ classes: 'add-to-cart ml-auto scale-125', html: addToCartSVG }).getElement();
+    const addButton = new ElementButtonCreator({ classes: 'add-to-cart ml-auto scale-125', html: addToCartSVG });
     productPricesBlock.appendNode(addButton);
 
     let lineItemId = this.consumer.cart?.lineItems.find((li) => li.productId === product.id)?.id;
     if (lineItemId) {
-      addButton.disabled = true;
+      addButton.getElement().disabled = true;
     }
 
-    addButton.addEventListener('click', async () => {
+    addButton.setHandler('click', async () => {
+      const loader = new Loader(card.getElement());
+      loader.showLoader();
+      addButton.addClass('pointer-events-none');
+
       if (!this.consumer.cart) {
         try {
-          this.consumer.cart = (await createCart(this.consumer.apiClient, { currency: 'USD' })).body;
-        } catch (err) {
-          if (err instanceof Error) {
-            if (err.message) {
-              new Message(err.message, 'error').showMessage();
-            } else {
-              new Message('Something went wrong. Try later.', 'error').showMessage();
-            }
-          }
+          this.consumer.cart = (await createMyCart(this.consumer.apiClient, { currency: 'USD' })).body;
+        } catch {
+          new Message('Something went wrong. Try later.', 'error').showMessage();
+          loader.hideLoader();
+          addButton.removeClass('pointer-events-none');
         }
       }
 
@@ -161,28 +152,30 @@ export class Goods {
 
       try {
         this.consumer.cart = (
-          await addToCart(this.consumer.apiClient, this.consumer.cart.version, this.consumer.cart.id, product.id)
+          await addToMyCart(this.consumer.apiClient, this.consumer.cart.version, this.consumer.cart.id, product.id)
         ).body;
+        addButton.getElement().disabled = true;
         lineItemId = this.consumer.cart.lineItems.find((li) => li.productId === product.id)?.id;
-        addButton.disabled = true;
         new Message('Product has been added to cart.', 'info').showMessage();
-      } catch (err) {
-        if (err instanceof Error) {
-          if (err.message) {
-            new Message(err.message, 'error').showMessage();
-          } else {
-            new Message('Something went wrong. Try later.', 'error').showMessage();
-          }
-        }
+      } catch {
+        new Message('Something went wrong. Try later.', 'error').showMessage();
       }
+      loader.hideLoader();
+      addButton.removeClass('pointer-events-none');
     });
 
     nameDescriptionBlock.appendNode(productNameBlock, productDescriptionBlock);
     infoBlock.appendNode(nameDescriptionBlock, productPricesBlock);
     card.appendNode(productImageBlock, infoBlock);
 
-    this.content.getElement().innerHTML = '';
-    this.content.appendNode(card);
+    image.setHandler('load', () => {
+      this.content.getElement().innerHTML = '';
+      this.content.appendNode(card);
+    });
+    image.setHandler('error', () => {
+      this.content.getElement().innerHTML = '';
+      this.content.appendNode(card);
+    });
   }
 
   async createCard(): Promise<void> {
